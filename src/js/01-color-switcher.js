5804dd1ce2b91293176d8ea0f9c34931afa12ae9
Denys Yefimkin <iefimkindenis@gmail.com>
2022-07-26 18:27:30 +0300

    Update Home Work 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let timerId = null;
+
+
+startButton.addEventListener('click', () => {
+    startButton.setAttribute('disabled', 'true');
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+
+stopButton.addEventListener('click', () => {
+    clearInterval(timerId);
+    startButton.removeAttribute('disabled')
+})
