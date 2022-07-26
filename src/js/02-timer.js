5a5b21639da3bf0bd3a1db1aa84ec489ef7738f6
Denys Yefimkin <iefimkindenis@gmail.com>
2022-07-26 22:43:09 +0300

    add flicker

---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const refs = {
+    input: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+}
